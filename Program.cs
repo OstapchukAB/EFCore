a73d96481b7bacbc543a625d5a95c66007df71c9
OstapchukAB <antbg@yandex.ru>
2022-11-02 17:49:24 +0300

    добавил методы редактирования и удаления данных

--- Program.cs
+++ Program.cs
@@ -4,20 +4,26 @@ public class Programm
 
         public static void Main()
         {
-                Console.WriteLine("Test EF Core");
+                Console.WriteLine("---Test EF Core---");
                 using (ApplicationContext db = new ApplicationContext())
                 {
                 
 
-                GetData(db,"до редактирования");
+                GetData(db,"---до добавления---");
 
                 User tom = new User { Name = "Tom", Age = 33 };
                 User alice = new User { Name = "Alice", Age = 26 };
-
                 AddData(db,new List<User>{tom,alice});
+                
+                GetData(db,"---после добавления---");
 
-                GetData(db,"после редактирования");
+                User NewUser = new User { Name = "New", Age = 100 };
+                EditDataForSelectId(db,NewUser,20);
+                
+                GetData(db,"---после редактирования---");
 
+                RemoveDataForListId(db,new List<int>{21,23,25,0});
+                 GetData(db,"----после удаления----");
 
                 }
 
@@ -43,8 +49,40 @@ public class Programm
                 
                 foreach(User u in ListUser) 
                         db.Users.Add(u);
-                        
+
                 db.SaveChanges();
 
+        }
+        public static void EditDataForSelectId (ApplicationContext db, User NewUser, int idForFind,  string? message=null)
+        {
+                if (message !=null )
+                        Console.WriteLine(message);
+                
+                User? user = db.Users.FirstOrDefault(x=>x.Id.Equals(idForFind));
+                if (user !=null)
+                {
+                        user.Age=NewUser.Age;
+                        user.Name=NewUser.Name;   
+                        db.SaveChanges(); 
+                }
+        }
+
+        public static void RemoveDataForListId (ApplicationContext db, List<int> ListIdForDelete,  string? message=null)
+        {
+                if (message !=null )
+                        Console.WriteLine(message);
+                        
+                List<User> ListUser= new List<User>();
+                foreach(int id in ListIdForDelete)
+                { 
+                        User? user = db.Users.FirstOrDefault(x=>x.Id.Equals(id));
+                        if (user !=null)
+                        ListUser.Add(user);  
+                }
+                foreach(User userForDelete in  ListUser)
+                {
+                        db.Users.Remove(userForDelete); 
+                        db.SaveChanges(); 
+                }
         }
 }
