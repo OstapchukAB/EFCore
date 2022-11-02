554da88a3496f5ac30ce1517990c11379b88887e
OstapchukAB <antbg@yandex.ru>
2022-11-02 17:09:01 +0300

    реализовал отдельные методы по загрузке данных

--- Program.cs
+++ Program.cs
@@ -7,13 +7,44 @@ public class Programm
                 Console.WriteLine("Test EF Core");
                 using (ApplicationContext db = new ApplicationContext())
                 {
-                    User tom = new User { Name = "Tom", Age = 33 };
-                    User alice = new User { Name = "Alice", Age = 26 };
                 
-                    // Добавление
-                    db.Users.Add(tom);
-                    db.Users.Add(alice);
-                    db.SaveChanges();
+
+                GetData(db,"до редактирования");
+
+                User tom = new User { Name = "Tom", Age = 33 };
+                User alice = new User { Name = "Alice", Age = 26 };
+
+                AddData(db,new List<User>{tom,alice});
+
+                GetData(db,"после редактирования");
+
+
                 }
+
+                Console.ReadKey();  
+                 
+        } 
+
+        public static void GetData (ApplicationContext db, string? message=null)
+        {
+                if (message !=null )
+                        Console.WriteLine(message);
+                
+                var users = db.Users.ToList();
+                  foreach (User u in users)
+                        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+
+        }
+
+         public static void AddData (ApplicationContext db, List<User> ListUser, string? message=null)
+        {
+                if (message !=null )
+                        Console.WriteLine(message);
+                
+                foreach(User u in ListUser) 
+                        db.Users.Add(u);
+                        
+                db.SaveChanges();
+
         }
 }
