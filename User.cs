c740548ad7b9efc2f7738c7ede136582bcc62ec2
OstapchukAB <antbg@yandex.ru>
2022-11-02 17:59:45 +0300

    ввел новый столбец DateTime с начальной инициацией в таблицу User через миграцию

--- User.cs
+++ User.cs
@@ -5,6 +5,8 @@ public class User
     public int Id { get; set; }
     public string? Name { get; set; }
     public int Age { get; set; }
+
+    public DateTime Dt {get;set;} =DateTime.Now;  
 }
 
 }
